0bd510c56b964d3b5504d17aae1fe11e136f30c5
Larissa <larissa@fantasticocomic.com>
2022-11-16 13:18:29 -0600

    Create cardimage component

---
+++ src/components/CardImage.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="cardimage">
+    <div class="cardimage__image" :style="item.image">
+      <span class="cardimage__quality">item.quality</span>
+      <span class="cardimage__duration">item.duration</span>
+    </div>
+    <div class="cardimage__description">item.description</div>
+    <div class="cardimage__tags">
+      <div v-for="tag in tags" :key="tag.id" class="cardimage__tag">
+        {{ tag.name }}
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "CardImage",
+
+  props: {
+    item: {
+      type: Object,
+    },
+  },
+};
+</script>
---
+++ src/mockdata/index.js
@@ -0,0 +1,283 @@
+export default {
+  cardsData: [
+    {
+      id: "prn001",
+      image: "",
+      quality: "4k",
+      duration: 4,
+      description:
+        "Ride Jada Steven's bubble butt, while she's on top of Abella Danger.",
+      tags: ["Jada Stevens", "Abella Danger", "Kiiroo"],
+    },
+    {
+      id: "prn002",
+      image: "",
+      quality: "hd",
+      duration: 12,
+      description:
+        "Rory Knox stretching Summer Vixen's ass with a special anal toy, to make room for that big cock of yours.",
+      tags: ["Rory Knox", "Summer Vixen", "Kiiroo"],
+    },
+    {
+      id: "prn003",
+      image: "",
+      quality: "vr",
+      duration: 2,
+      description:
+        "Rachel Starr and Ava Addams double-team your cock to give you a blow job you won't forget.",
+      tags: ["Rachek Starr", "Ava Addams", "Kiiroo"],
+    },
+
+    {
+      id: "prn004",
+      image: "",
+      quality: "4k",
+      duration: 6,
+      description:
+        "Melissa Moore and Riley Reid give you a special strip tease after taking tennis lessons.",
+      tags: ["Melissa Moore", "Riley reid", "Kiiroo"],
+    },
+    {
+      id: "prn005",
+      image: 12,
+      quality: "hd",
+      duration: 12,
+      description:
+        "Rory Knox stretching Summer Vixen's ass with a special anal toy, to make room for that big cock of yours.",
+      tags: ["Rory Knox", "Summer Vixen", "Kiiroo"],
+    },
+    {
+      id: "prn006",
+      image: "",
+      quality: "hd",
+      duration: 5,
+      description:
+        "Have a four-way with Angel Gostosa, Lilly Bell, and Spencer Bradley during spring break.",
+      tags: ["Angel Gostosa", "Lilly Bell", "Spencer Bradley"],
+    },
+
+    {
+      id: "prn007",
+      image: "",
+      quality: "4k",
+      duration: 5,
+      description:
+        "Ride Jada Steven’s bubble butt, while she’s on top of Abella Danger.",
+      tags: ["Ava Addams", "Kiiroo", "Handy"],
+    },
+    {
+      id: "prn008",
+      image: "",
+      quality: "4k",
+      duration: 10,
+      description:
+        "Rory Knox stretching Summer Vixen’s ass with a special anal toy, to make room for that big cock of yours.",
+      tags: ["Anissa Kate", "Giana Dior", "Handy"],
+    },
+    {
+      id: "prn009",
+      image: "",
+      quality: "vr",
+      duration: 2,
+      description:
+        "Peta Jensen is hanging out at the beach when she notices you checking her out...",
+      tags: ["Peta Jensen", "Kiiroo", "Handy"],
+    },
+
+    {
+      id: "prn010",
+      image: "",
+      quality: "4k",
+      duration: 4,
+      description:
+        "Ride Jada Steven's bubble butt, while she's on top of Abella Danger.",
+      tags: ["Jada Stevens", "Abella Danger", "Kiiroo"],
+    },
+    {
+      id: "prn011",
+      image: "",
+      quality: "hd",
+      duration: 12,
+      description:
+        "Rory Knox stretching Summer Vixen's ass with a special anal toy, to make room for that big cock of yours.",
+      tags: ["Rory Knox", "Summer Vixen", "Kiiroo"],
+    },
+    {
+      id: "prn012",
+      image: "",
+      quality: "vr",
+      duration: 2,
+      description:
+        "Rachel Starr and Ava Addams double-team your cock to give you a blow job you won't forget.",
+      tags: ["Rachek Starr", "Ava Addams", "Kiiroo"],
+    },
+
+    {
+      id: "prn013",
+      image: "",
+      quality: "4k",
+      duration: 6,
+      description:
+        "Melissa Moore and Riley Reid give you a special strip tease after taking tennis lessons.",
+      tags: ["Melissa Moore", "Riley reid", "Kiiroo"],
+    },
+    {
+      id: "prn014",
+      image: 12,
+      quality: "hd",
+      duration: 12,
+      description:
+        "Rory Knox stretching Summer Vixen's ass with a special anal toy, to make room for that big cock of yours.",
+      tags: ["Rory Knox", "Summer Vixen", "Kiiroo"],
+    },
+    {
+      id: "prn015",
+      image: "",
+      quality: "hd",
+      duration: 5,
+      description:
+        "Have a four-way with Angel Gostosa, Lilly Bell, and Spencer Bradley during spring break.",
+      tags: ["Angel Gostosa", "Lilly Bell", "Spencer Bradley"],
+    },
+
+    {
+      id: "prn016",
+      image: "",
+      quality: "4k",
+      duration: 5,
+      description:
+        "Ride Jada Steven’s bubble butt, while she’s on top of Abella Danger.",
+      tags: ["Ava Addams", "Kiiroo", "Handy"],
+    },
+    {
+      id: "prn017",
+      image: "",
+      quality: "4k",
+      duration: 10,
+      description:
+        "Rory Knox stretching Summer Vixen’s ass with a special anal toy, to make room for that big cock of yours.",
+      tags: ["Anissa Kate", "Giana Dior", "Handy"],
+    },
+    {
+      id: "prn018",
+      image: "",
+      quality: "vr",
+      duration: 2,
+      description:
+        "Peta Jensen is hanging out at the beach when she notices you checking her out...",
+      tags: ["Peta Jensen", "Kiiroo", "Handy"],
+    },
+
+    {
+      id: "prn019",
+      image: "",
+      quality: "4k",
+      duration: 4,
+      description:
+        "Ride Jada Steven's bubble butt, while she's on top of Abella Danger.",
+      tags: ["Jada Stevens", "Abella Danger", "Kiiroo"],
+    },
+    {
+      id: "prn020",
+      image: "",
+      quality: "hd",
+      duration: 12,
+      description:
+        "Rory Knox stretching Summer Vixen's ass with a special anal toy, to make room for that big cock of yours.",
+      tags: ["Rory Knox", "Summer Vixen", "Kiiroo"],
+    },
+    {
+      id: "prn021",
+      image: "",
+      quality: "vr",
+      duration: 2,
+      description:
+        "Rachel Starr and Ava Addams double-team your cock to give you a blow job you won't forget.",
+      tags: ["Rachek Starr", "Ava Addams", "Kiiroo"],
+    },
+
+    {
+      id: "prn022",
+      image: "",
+      quality: "4k",
+      duration: 6,
+      description:
+        "Melissa Moore and Riley Reid give you a special strip tease after taking tennis lessons.",
+      tags: ["Melissa Moore", "Riley reid", "Kiiroo"],
+    },
+    {
+      id: "prn023",
+      image: 12,
+      quality: "hd",
+      duration: 12,
+      description:
+        "Rory Knox stretching Summer Vixen's ass with a special anal toy, to make room for that big cock of yours.",
+      tags: ["Rory Knox", "Summer Vixen", "Kiiroo"],
+    },
+    {
+      id: "prn024",
+      image: "",
+      quality: "hd",
+      duration: 5,
+      description:
+        "Have a four-way with Angel Gostosa, Lilly Bell, and Spencer Bradley during spring break.",
+      tags: ["Angel Gostosa", "Lilly Bell", "Spencer Bradley"],
+    },
+
+    {
+      id: "prn025",
+      image: "",
+      quality: "4k",
+      duration: 5,
+      description:
+        "Ride Jada Steven’s bubble butt, while she’s on top of Abella Danger.",
+      tags: ["Ava Addams", "Kiiroo", "Handy"],
+    },
+    {
+      id: "prn026",
+      image: "",
+      quality: "4k",
+      duration: 10,
+      description:
+        "Rory Knox stretching Summer Vixen’s ass with a special anal toy, to make room for that big cock of yours.",
+      tags: ["Anissa Kate", "Giana Dior", "Handy"],
+    },
+    {
+      id: "prn027",
+      image: "",
+      quality: "vr",
+      duration: 2,
+      description:
+        "Peta Jensen is hanging out at the beach when she notices you checking her out...",
+      tags: ["Peta Jensen", "Kiiroo", "Handy"],
+    },
+
+    {
+      id: "prn028",
+      image: "",
+      quality: "4k",
+      duration: 5,
+      description:
+        "Ride Jada Steven’s bubble butt, while she’s on top of Abella Danger.",
+      tags: ["Ava Addams", "Kiiroo", "Handy"],
+    },
+    {
+      id: "prn029",
+      image: "",
+      quality: "4k",
+      duration: 10,
+      description:
+        "Rory Knox stretching Summer Vixen’s ass with a special anal toy, to make room for that big cock of yours.",
+      tags: ["Anissa Kate", "Giana Dior", "Handy"],
+    },
+    {
+      id: "prn030",
+      image: "",
+      quality: "vr",
+      duration: 2,
+      description:
+        "Peta Jensen is hanging out at the beach when she notices you checking her out...",
+      tags: ["Peta Jensen", "Kiiroo", "Handy"],
+    },
+  ],
+};
